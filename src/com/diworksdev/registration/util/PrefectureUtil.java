472d72dc5462955046b4018f8d35bd059ea3347f
saho <jyako.t@gmail.com>
2026-04-17 08:38:08 +0900

    都道府県一覧をUtil化

---
+++ src/com/diworksdev/registration/util/PrefectureUtil.java
@@ -0,0 +1,62 @@
+package com.diworksdev.registration.util;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class PrefectureUtil {
+
+	public static Map<String, String> getPrefectureMap() {
+
+        Map<String, String> map = new LinkedHashMap<>();
+
+        map.put("1", "北海道");
+        map.put("2", "青森県");
+        map.put("3", "岩手県");
+        map.put("4", "宮城県");
+        map.put("5", "秋田県");
+        map.put("6", "山形県");
+        map.put("7", "福島県");
+        map.put("8", "茨城県");
+        map.put("9", "栃木県");
+        map.put("10", "群馬県");
+        map.put("11", "埼玉県");
+        map.put("12", "千葉県");
+        map.put("13", "東京都");
+        map.put("14", "神奈川県");
+        map.put("15", "新潟県");
+        map.put("16", "富山県");
+        map.put("17", "石川県");
+        map.put("18", "福井県");
+        map.put("19", "山梨県");
+        map.put("20", "長野県");
+        map.put("21", "岐阜県");
+        map.put("22", "静岡県");
+        map.put("23", "愛知県");
+        map.put("24", "三重県");
+        map.put("25", "滋賀県");
+        map.put("26", "京都府");
+        map.put("27", "大阪府");
+        map.put("28", "兵庫県");
+        map.put("29", "奈良県");
+        map.put("30", "和歌山県");
+        map.put("31", "鳥取県");
+        map.put("32", "島根県");
+        map.put("33", "岡山県");
+        map.put("34", "広島県");
+        map.put("35", "山口県");
+        map.put("36", "徳島県");
+        map.put("37", "香川県");
+        map.put("38", "愛媛県");
+        map.put("39", "高知県");
+        map.put("40", "福岡県");
+        map.put("41", "佐賀県");
+        map.put("42", "長崎県");
+        map.put("43", "熊本県");
+        map.put("44", "大分県");
+        map.put("45", "宮崎県");
+        map.put("46", "鹿児島県");
+        map.put("47", "沖縄県");
+
+        return map;
+    }
+}
